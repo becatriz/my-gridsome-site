ab8f166cea3b603be9e9ed4856002453482ac0a1
Rebeca Lopes <rebeca.cruz@before.com.br>
2021-10-19 22:09:31 -0400

    feat: commit inicial

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,23 +2,7 @@
   <Layout>
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <h2>Manual Blogroll</h2>
-
-    <ul>
-      <li><g-link to="/blog-html/blog-post-one">Blog Post One</g-link></li>
-    </ul>
-
-    <h2>Automatic Blogroll</h2>
-
+    <h2>Postagens</h2>
     <ul>
       <li v-for="post in $page.posts.edges" :key="post.id">
         <g-link :to="post.node.path">
@@ -26,18 +10,6 @@
         </g-link>
       </li>
     </ul>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs" target="_blank" rel="noopener"
-        >Gridsome Docs</a
-      >
-      <a
-        href="https://github.com/gridsome/gridsome"
-        target="_blank"
-        rel="noopener"
-        >GitHub</a
-      >
-    </p>
   </Layout>
 </template>
 
